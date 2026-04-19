3d3590f409d08bf7b345993ad92f1e0625d2d5b9
jaimerodri77 <68249921+jaimerodri77@users.noreply.github.com>
2026-04-18 22:23:41 -0500

    Update scraper_diario.py

--- scraper_diario.py
+++ scraper_diario.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import logging
 import os
 from playwright.sync_api import sync_playwright
-
+from playwright_stealth import stealth_sync
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 CARPETA_SALIDA = "datos"
@@ -92,7 +92,12 @@ if __name__ == "__main__":
     
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
-        page = browser.new_context().new_page()
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+            viewport={"width": 1920, "height": 1080}
+        )
+        page = context.new_page()
+        stealth_sync(page)
         page.goto("https://www.sofascore.com/tennis")
         page.wait_for_timeout(2000)
         
